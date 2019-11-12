b25f2c5f668f4aae1f636d794a34251753ab4e28
Sona <sona_gevorgyan2@edu.aua.am>
2019-11-12 16:15:55 +0400

    Final project

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+const reader = require("readline-sync");
 const util = require("./myUtils.js");
 
-console.log(util.isPrime(2));
+num1 = reader.questionInt("Please insert your number1 = ");
+num2 = reader.questionInt("Please insert your number2 = ");
 
+util.includeRange(num1, num2);
--- myUtils.js
+++ myUtils.js
@@ -1,13 +1,13 @@
 module.exports = {
-	isPrime: function(n) {
-		if(n <=1) {
+	isPrime: function(num) {
+		if(num <=1) {
 			return false;
 		}
 
 		let div = 2;
 
-		while(div <= Math.sqrt(n)) {
-			if(n % div === 0) {
+		while(div <= Math.sqrt(num)) {
+			if(num % div === 0) {
 				return false;
 			}
 
@@ -15,5 +15,40 @@ module.exports = {
 		}
 
 		return true;
+	},
+
+	includeRange: function (num1, num2) {
+		let rangeArray = [];
+		let rangePrime= [];
+		let maxnum;
+		let minnum;
+		if (num1>num2) {
+			maxnum=num1;
+			minnum=num2;
+		}
+		else {
+			maxnum=num2;
+			minnum=num1;
+			
+		}
+
+			while (maxnum>= minnum) {
+				rangeArray.push(maxnum);
+				maxnum--;
+				}
+	
+		for(let i=0; i < rangeArray.length; i++) {
+		if(this.isPrime(rangeArray[i]) ) {  
+			rangePrime.push(rangeArray[i]);
+		      }
+		 }
+		  console.log(rangePrime);
+		}
 	}
-}
+		
+
+
+    
+	
+	
+   
